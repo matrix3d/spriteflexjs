fb839df5b49b8fe2a3d7c36ddbae9cd51e91643c
matrix3d <405628079@qq.com>
2018-06-04 23:14:41 +0800

    -warn-public-vars=false

--- test/build_fd.bat
+++ test/build_fd.bat
@@ -3,7 +3,7 @@ for /F tokens^=2^,3^,5delims^=^<^"^= %%a in (%2.as3proj) do (
    if "%%a" equ "compile path" set main=%%b
 )
 
-set ARGS= -external-library-path+=lib\* -source-path+=../src -js-compiler-option="--compilation_level SIMPLE_OPTIMIZATIONS" -js-output-optimization=skipAsCoercions -remove-circulars
+set ARGS=-warn-public-vars=false -external-library-path+=lib\* -source-path+=../src -js-compiler-option="--compilation_level SIMPLE_OPTIMIZATIONS" -js-output-optimization=skipAsCoercions -remove-circulars
 
 if %3==debug (
 	set ARGS=%ARGS% -debug=true -source-map
